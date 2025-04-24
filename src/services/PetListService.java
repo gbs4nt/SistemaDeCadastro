26fc266b2b96995f170575a33c3389d33b10788e
unknown <glima9719@gmail.com>
2025-04-24 09:45:09 -0300

    feat:Implementação do método de listar os pets

--- src/services/PetListService.java
+++ src/services/PetListService.java
@@ -1,4 +1,27 @@
 package services;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PetListService {
+    public static void listPets(){
+        File dir = new File("C:\\Users\\Gabriel\\Documents\\Estudos\\DesafioCadastro\\desafioCadastro\\src\\registredPets");
+        File[] arquivos = dir.listFiles();
+        if(arquivos==null){
+            System.out.println("Nenhum arquivo encontrado");
+            return;
+        }
+
+        List<File> pets = new ArrayList<>();
+
+        for(File arquivo: arquivos){
+            if(arquivo.getName().endsWith(".txt")){
+                pets.add(arquivo);
+            }
+        }
+        for(File petlist:pets){
+            System.out.println(petlist.getName());
+        }
+    }
 }
